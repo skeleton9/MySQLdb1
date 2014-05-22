PyMySQL/mysqlclient

commit 87d1145c0d6ee4f5a8ecf6d5c62d2479b9cf27ea

Fix the conversion of list or tuple args to a SQL.
When there is one element on the list, the generated SQL was
(1,) (python notation of a single element tuple, which is not
valid in SQL.

MySQLdb/cursors.py
@@ -180,7 +180,10 @@ class BaseCursor(object):
         if isinstance(query, unicode):
             query = query.encode(db.unicode_literal.charset)
         if args is not None:
-            query = query % db.literal(args)
+            if isinstance(args, dict):
+                query = query % {key: db.literal(item) for key, item in args.iteritems()}
+            else:
+                query = query % tuple([db.literal(item) for item in args])
         try:
             r = None
             r = self._query(query)

MySQLdb/converters.py
@@ -129,13 +129,16 @@ def char_array(s):
 def array2Str(o, d):
     return Thing2Literal(o.tostring(), d)
 
+def quote_tuple(t, d):
+    return "(%s)" % (','.join(escape_sequence(t, d)))
+
 conversions = {
     IntType: Thing2Str,
     LongType: Long2Int,
     FloatType: Float2Str,
     NoneType: None2NULL,
-    TupleType: escape_sequence,
-    ListType: escape_sequence,
+    TupleType: quote_tuple,
+    ListType: quote_tuple,
     DictType: escape_dict,
     InstanceType: Instance2Str,
     ArrayType: array2Str,

skeleton9/MySQLdb1

commit 171d335b8a9673a745f11d622163c467ab035d24

fix string replacement support for sql
string replacement was broken by
https://github.com/farcepest/MySQLdb1/commit/87d1145c0d6ee4f5a8ecf6d5c62d2479b9cf27ea

This fix is to bring the string replacement support back:
  sql = "SELECT * FROM table_name WHERE column = %s"
  cursor.execute(sql, 'passed_variable')

MySQLdb/cursors.py
@@ -183,6 +183,8 @@ class BaseCursor(object):
             if isinstance(args, dict):
                 query = query % dict((key, db.literal(item))
                                      for key, item in args.iteritems())
+            elif isinstance(args, str) or isinstance(args, unicode):
+                query = query % db.literal(args)
             else:
                 query = query % tuple([db.literal(item) for item in args])
         try: